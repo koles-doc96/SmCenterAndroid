6f6262d13b37f32a5133a3973b0dbd8404452ae1
koles-doc <koles.doc96.09.27@gmail.com>
2019-06-19 16:00:50 +0700

    Вынес все константы в отдельный клас

---
+++ app/src/main/java/ru/kolesnikov/sm_center/Constants/Constant.java
@@ -0,0 +1,17 @@
+package ru.kolesnikov.sm_center.Constants;
+
+public class Constant {
+    public static final String DB_NAME = "myDb";
+    public static final String LOG_TAG = "SMLogs";
+    public static final String NAME = "name";
+    public static final String VALUE = "value";
+    public static final String SAVE_TABLE = "saveTable";
+    public static final String ID = "id";
+
+    public static final String APP_PREFERENCES = "mysettings";
+    public static final String SEMICOLON = ";";
+    public static final String NEW_STRING = "\n";
+
+    public static final String APP_PREFERENCES_PHONE = "phone";
+    public static final String SAVE_CLASS_NAME = "className";
+}
